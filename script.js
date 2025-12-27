771e24fd7b66722c167e8a605f707ff2963d6c64
robertgomezbatte-svg <robertgomezbatte@gmail.com>
2025-12-27 19:35:50 +0100

    Fix typo in variable declaration

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-Vconst { createFFmpeg, fetchFile } = FFmpeg;
+const { createFFmpeg, fetchFile } = FFmpeg;
 const ffmpeg = createFFmpeg({ log: true });
 
 const videoInput = document.getElementById("videoInput");
